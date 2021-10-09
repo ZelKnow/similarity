feab1c04feb9fb90129dbebe2f0b4be81921f1b0
ZelKnow <shenghuang@stu.pku.edu.cn>
2021-10-09 21:36:43 +0800

    ✨ feat: init vsm

---
+++ vsm.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File          : vsm.py
+@Description   : 实现向量空间模型
+@Date          : 2021/10/09 21:33:47
+@Author        : ZelKnow
+@Github        : https://github.com/ZelKnow
+"""
+__author__ = "ZelKnow"
+
+
+class VectorSpaceModel:
+    def __init__(self):
+        pass
